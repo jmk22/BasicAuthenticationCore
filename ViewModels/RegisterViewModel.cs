6ff84e968bb12f97a7172ef154c7f0abc924406f
jill kuchman <pillanoid@gmail.com>
2016-09-08 09:04:42 -0700

    initial commit

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name =)]
+    }
+}
