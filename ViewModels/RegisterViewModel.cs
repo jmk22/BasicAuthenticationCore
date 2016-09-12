0ac01aa33ab34542113d373a20f476695caa5e60
jill kuchman <pillanoid@gmail.com>
2016-09-12 15:49:33 -0700

    Add RegisterViewModel and initial migration

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BasicAuthentication.ViewModels
 {
@@ -15,6 +11,12 @@ namespace BasicAuthentication.ViewModels
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name =)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
